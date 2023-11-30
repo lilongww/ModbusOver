9d5e70ead53cb2dc5a46bfa85f4be2cbb7423e31
lilong <634916105@qq.com>
2023-11-30 14:45:37 +0800

    export package

--- cmake/ExportHelper.cmake
+++ cmake/ExportHelper.cmake
@@ -1,3 +1,22 @@
+function(export_config target_name installdir includedir)
+    include(CMakePackageConfigHelpers)
+
+    configure_package_config_file("${PROJECT_SOURCE_DIR}/cmake/${target_name}Config.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/${target_name}Config.cmake"
+        INSTALL_DESTINATION "${installdir}/cmake/"
+        PATH_VARS installdir includedir)
+
+    write_basic_package_version_file(
+        "${CMAKE_CURRENT_BINARY_DIR}/${target_name}ConfigVersion.cmake"
+        VERSION ${PROJECT_VERSION}
+        COMPATIBILITY AnyNewerVersion)
+
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${target_name}ConfigVersion.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${target_name}Config.cmake" DESTINATION "${installdir}/cmake/")
+    set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
+    export(PACKAGE ${target_name})
+endfunction()
+
 function(export_install_target target_name includedir libdir bindir)
     install(TARGETS ${target_name}
         EXPORT ${target_name}
